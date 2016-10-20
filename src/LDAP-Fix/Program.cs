9113c661aefba7d05d87430c410201301dc58ad0
Stephan Reichinger <stephan.reichinger@xitaso.com>
2016-10-20 15:34:36 +0200

    fixed issue that docker could not find nuget package

---
+++ src/LDAP-Fix/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Novell.Directory.Ldap;
+
+namespace LDAP_Fix
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            int LdapPort = 10389;
+            int searchScope = LdapConnection.SCOPE_ONE;
+            int LdapVersion = LdapConnection.Ldap_V3; ;
+            bool attributeOnly = true;
+            String[] attrs = { LdapConnection.NO_ATTRS };
+            String ldapHost = "192.168.2.130";
+            String loginDN = "";
+            String password = "";
+            String searchBase = "";
+            String searchFilter = "";
+            LdapConnection lc = new LdapConnection();
+
+            try
+            {
+                // connect to the server
+                lc.Connect(ldapHost, LdapPort);
+                // bind to the server
+                lc.Bind(LdapVersion, loginDN, password);
+
+                LdapSearchResults searchResults =
+                    lc.Search(searchBase,      // container to search
+                    searchScope,     // search scope
+                    searchFilter,    // search filter
+                    attrs,           // "1.1" returns entry name only
+                    attributeOnly);  // no attributes are returned
+
+                // print out all the objects
+                while (searchResults.hasMore())
+                {
+                    LdapEntry nextEntry = null;
+                    try
+                    {
+                        nextEntry = searchResults.next();
+                    }
+                    catch (LdapException e)
+                    {
+                        Console.WriteLine("Error: " + e.ToString());
+
+                        // Exception is thrown, go for next entry
+                        continue;
+                    }
+
+                    Console.WriteLine("\n" + nextEntry.DN);
+                }
+                // disconnect with the server
+                lc.Disconnect();
+            }
+            catch (LdapException e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ src/LDAP-Fix/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LDAP_Fix")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("65cf5b6e-13cd-4238-bd16-1b4cdbc7ab04")]
